378192d579c798be47bcc8d934f3ed89488e318a
Barkat Ali <barkat@unifiedinfotech.net>
2021-01-19 21:17:27 +0530

    added 1st

---
+++ precache-manifest.3522a6056df07712159db5ef33202d1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd51edd63cb015da532c04a92d01b656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2171416c9dd2adce76e2",
+    "url": "/static/css/main.a3528f2b.chunk.css"
+  },
+  {
+    "revision": "e8cfb6d79923bcb5d7dc",
+    "url": "/static/js/2.7c6288c9.chunk.js"
+  },
+  {
+    "revision": "2171416c9dd2adce76e2",
+    "url": "/static/js/main.e9c03732.chunk.js"
+  },
+  {
+    "revision": "6544cff1e84c18c8e3db",
+    "url": "/static/js/runtime-main.d7136c8a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/logo.b8503d26.png"
+  }
+]);
